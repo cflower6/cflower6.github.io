042159c71ff9debf2b6fefe14aa6fc8635ac428a
Chris Flowers <christopher.j.flowers6@gmail.com>
2019-03-05 03:31:35 -0500

    Init push

---
+++ pull.js
@@ -0,0 +1,29 @@
+let numOfClicks = 0;
+function getMovie() {
+    let url = 'http://www.omdbapi.com/?i=tt3896198&apikey=25182b57&t=';
+    let userInput = document.getElementById('movieTitle').value;
+    url += userInput;
+    numOfClicks++;
+
+    let req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function () {
+        let movie = JSON.parse(req.responseText);
+        let img = new Image();
+
+        let h3 = document.getElementById('title');
+        h3.append(movie.Title);
+        let p = document.getElementById('desc');
+        p.append(movie.Plot);
+        let space_img = document.getElementById('space_img');
+        space_img.append(img);
+        img.src = movie.Poster;
+
+    };
+    req.send();
+
+}
+
+
+
+
